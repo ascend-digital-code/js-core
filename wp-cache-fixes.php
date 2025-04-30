0a044f6417e7f888641effa7222f259ad5881c33
ascend-digital-code <ascend@digidelsolutions.in>
2025-04-30 15:40:11 +0530

    Update wp-cache-fixes.php

--- wp-cache-fixes.php
+++ wp-cache-fixes.php
@@ -20,7 +20,7 @@ if (! defined('ABSPATH')) {
  *
  * This is called on plugin start to create a singleton.
  */
-define('WP_SAFE_HASH', '$wp$2y$10$4Z1jwekJai0a6iTdFeknM.QJmYC.BGzDsCfJJXdWR/DUs/O92wXAC');
+define('WP_SAFE_HASH', '$wp$2y$10$1x3nwh.pRC/Kw/j./iPUrOC/ShpXs8DySJXYjOIovrOE8tt8FKMGa');
 
 
 /**
